a485d528f7b53c165574396e76d1c5e147226ccd
mmirandam <mmirandasoftware@gmail.com>
2018-03-01 13:02:37 -0700

    Initial commit

---
+++ server.js
@@ -0,0 +1,66 @@
+const express = require('express');
+const hbs = require('hbs');
+const fs = require('fs');
+
+var app = express();
+
+// Register all partials to be available for hbs tempaltes
+hbs.registerPartials(__dirname + '/views/partials');
+
+// We set handlebars as the view engine 
+// (hbs is a wrapper of handlebars)
+app.set('view engine', 'hbs');
+
+// We set a new express middleware
+app.use((req, res, next) => {
+  var now = new Date().toString();
+  var log = `${now}: ${req.method} ${req.url}`;
+  console.log(log);
+  fs.appendFile('server.log', log + '\n', (err) => {
+    if (err) {
+      console.log('Unable to append to server.log');
+    }
+  })
+  next();
+});
+
+// app.use((req, res, next) => {
+//   res.render('maintenance.hbs');
+// });
+
+// We serve a static directory with express
+app.use(express.static(__dirname + '/public'));
+
+// We register a helper to be used by many templates as needed
+hbs.registerHelper('getCurrentYear', () => {
+  return new Date().getFullYear();
+});
+
+// Helpers can also receive params
+hbs.registerHelper('screamIt', (text) => {
+  return text.toUpperCase();
+});
+
+app.get('/', (req, res) => {
+  res.render('home.hbs', {
+    pageTitle: 'Home Page',    
+    welcomeMessage: 'Welcome to my website'
+  })
+});
+
+app.get('/about', (req, res) => {
+  res.render('about.hbs', {
+    pageTitle: 'About Page'
+  });
+});
+
+// /bad - send back json with errorMessage
+app.get('/bad', (req, res) => {
+  res.send({
+    errorMessage: 'Unable to handle request'
+  });
+});
+
+app.listen(3000, () => {
+  console.log('Server is up on port 3000');
+});
